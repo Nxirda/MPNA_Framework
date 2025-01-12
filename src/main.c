e352c96d327a8279754f5c45220973e184fe035a
Nxirda <Nxirda@users.noreply.github.com>
2025-01-12 15:06:11 +0100

    Quick cl handler for the main

--- src/main.c
+++ src/main.c
@@ -5,11 +5,25 @@
 
 int main(int argc, char **argv)
 {
-
+    
+    if(argc != 3 && argc != 1)
+    {
+        fprintf(stderr, "Usage is %s <mesh_size> <nb_iter>\n", argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+    
     usz mesh_size = 4;
     u64 nb_bytes = mesh_size * mesh_size;
     u64 iter = 30;
+    
+    if(argc == 3)
+    {
+        mesh_size   = strtol(argv[1], NULL, 10);
+        nb_bytes    = mesh_size * mesh_size;
+        iter        = strtol(argv[2], NULL, 10);
+    }
 
+    /* Random target params */
     f64 min = 10e-12;
     f64 max = 10e12;
 
@@ -22,17 +36,17 @@ int main(int argc, char **argv)
     vector_t x_csr;
     allocate_vector(&x_csr, nb_bytes);
 
-    printf("====== Matrix in CSR storage format ======\n");
+    //printf("====== Matrix in CSR storage format ======\n");
     csr_matrix_t csr;
     allocate_CSR(mesh_size, &csr);
     fill_CSR(mesh_size, &csr);
-    print_CSR(&csr);
+    //print_CSR(&csr);
    
-    printf("====== Matrix in general storage format ======\n");
+    //printf("====== Matrix in general storage format ======\n");
     matrix_t general;
     allocate_matrix(mesh_size, &general);
     fill_matrix(mesh_size, general);
-    print_matrix(general);
+    //print_matrix(general);
 
     init_random_vector(&b, min, max);
 /******************************************************************************/
@@ -98,5 +112,5 @@ int main(int argc, char **argv)
     free_vector(&b);
 
     exit(EXIT_SUCCESS);
-    return 0;
+    //return 0;
 }
